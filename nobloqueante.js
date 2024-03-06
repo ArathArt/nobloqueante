97acc5a17693a9e2a28b74a05409e5a261072291
ArathArt <arathbactel@gmail.com>
2024-03-06 09:35:02 -0600

    primer commit nobloqueante.js

---
+++ nobloqueante.js
@@ -0,0 +1,21 @@
+'use strict'
+
+setTimeout(function(){
+    console.log('Tarea 1');
+},3000); //espera 3 seg
+
+setTimeout(function(){
+    console.log('Tarea 2');
+},20000); //espera 20 seg
+
+setTimeout(function(){
+    console.log('Tarea 3');
+},10000); //espera 10 seg
+
+setTimeout(function(){
+    console.log('Tarea 4');
+},2000); //espera 2 seg
+
+console.log('Tarea5');
+console.log(process.uptime())
+console.log('Fin del programa')
